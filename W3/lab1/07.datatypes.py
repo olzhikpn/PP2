6373c4beb27dcb4b0b222695cb99d9db1195bb5b
Olzhas <o_nasseken@kbtu.kz>
2025-02-10 01:06:54 +0500

    0

---
+++ W3/lab1/07.datatypes.py
@@ -0,0 +1,88 @@
+#Built-in Data Types
+
+
+#In programming, data type is an important concept.
+
+#Variables can store data of different types, and different types can do different things.
+
+#Python has the following data types built-in by default, in these categories:
+
+"""
+Text Type:	str
+"""
+
+"""
+Numeric Types:	int, float, complex
+"""
+
+"""
+Sequence Types:	list, tuple, range
+"""
+
+"""
+Mapping Type:	dict
+"""
+
+"""
+Set Types:	set, frozenset
+"""
+
+"""
+Boolean Type:	bool
+"""
+
+""""
+Binary Types:	bytes, bytearray, memoryview
+"""
+
+"""
+None Type:	NoneType
+"""
+
+
+#Getting the Data Type
+
+#You can get the data type of any object by using the type() function:
+
+#Example
+
+#Print the data type of the variable x:
+
+"""
+x = 5
+print(type(x))
+"""
+
+
+#Setting the Data Type
+
+#In Python, the data type is set when you assign a value to a variable:
+
+#Example
+
+"""x='Hello world', datatype is <str>"""
+
+"""x='20', datatype is <int>"""
+
+"""x='20,5', datatype is <float>"""
+
+"""x='20j', datatype is <complex>"""
+
+"""x=["apple", "banana", "cherry"], datatype is <list>"""
+
+"""x = ("apple", "banana", "cherry"), datatype is <	tuple>"""
+
+"""x=range(6), datatype is <range>"""
+
+"""x = {"name" : "John", "age" : 36}, datatype is <dict>"""
+
+"""x = {"apple", "banana", "cherry"}, datatype is <set>"""
+
+
+
+
+
+
+
+
+
