8ac0193d3a1e326ff0ae081c3144b3e28f12e568
Olzhas <o_nasseken@kbtu.kz>
2025-02-10 03:16:06 +0500

    0

---
+++ W3/lab1/06.variable/03.Assignmultipllevalues.py
@@ -0,0 +1,24 @@
+#1.Many Values to Multiple Variables
+
+#Example 1
+x, y, z = "a", "b", "c"
+print(x)
+print(y)
+print(z)
+
+#2.One Value to Multiple Variables
+
+#Example 1
+x = y = z = "a"
+print(x)
+print(y)
+print(z)
+
+#3.Unpack a Collection
+
+#Example 1
+letter = ["a", "b", "c"]
+x, y, z = letter
+print(x)
+print(y)
+print(z)
