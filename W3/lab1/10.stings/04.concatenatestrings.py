a64e800502e106b801ec7e7741869c823e246e73
Olzhas <o_nasseken@kbtu.kz>
2025-02-10 03:36:55 +0500

    0

---
+++ W3/lab1/10.stings/04.concatenatestrings.py
@@ -0,0 +1,15 @@
+#1.String Concatenation
+
+#To concatenate, or combine, two strings you can use the + operator.
+
+#Example 1
+a = "Hello"
+b = "Python!"
+c = a + b
+print(c)
+
+#Example 2
+a = "Hello"
+b = "Python!"
+c = a + " " + b
+print(c)
