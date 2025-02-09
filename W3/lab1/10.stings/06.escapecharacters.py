b665caed17f58ee567e86c13f1f2a5dcb56ef8c9
Olzhas <o_nasseken@kbtu.kz>
2025-02-10 03:40:39 +0500

    0

---
+++ W3/lab1/10.stings/06.escapecharacters.py
@@ -0,0 +1,25 @@
+#1.Escape Character
+
+#To insert characters that are illegal in a string, use an escape character '\'.
+
+#Error Example
+"""
+txt = "a "b" c"
+"""
+
+#To fix this problem, use the escape character \":
+
+#Example 1
+txt = "a \"b\" c"
+
+"""
+\'     Single Quote 
+\\     Backslash 
+\n     New Line 
+\r     Carriage Return 
+\t     Tab 
+\b     Backspace 
+\f     Form Feed 
+\ooo Octal value 
+\xhh Hex value
+"""
