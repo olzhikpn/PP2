877bb166d0f5d9409d0b87db80ab854f2b8a569b
Olzhas <o_nasseken@kbtu.kz>
2025-02-10 03:09:58 +0500

    0

---
+++ W3/lab1/06.variable/01.variables.py
@@ -0,0 +1,45 @@
+#1.Creating Variables
+
+#Example 1
+x = 1
+y = "Apple"
+print(x)
+print(y)
+
+#Example 2
+x = 2        # x is of type int
+x = "Banana" # x is now of type str
+print(x)
+
+#2.Casting
+
+#Example 1
+x = str(4)    # x will be '4'
+y = int(4)    # y will be 4
+z = float(4)  # z will be 4.0
+
+#3.Get the Type
+
+#You can get the data type of a variable with the type() function.
+
+#Example 1
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+#4.Single or Double Quotes?
+
+#You can use both variants.
+
+#Example 1
+x = "Apple"
+# is the same as
+x = 'Apple'
+
+#5.Case-Sensitive
+
+#Example 1
+a = 3
+A = "Apple"
+#A will not overwrite a
