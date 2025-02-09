390ccb261e82199db07ffb3b19885bc9cfbc4aad
Olzhas <o_nasseken@kbtu.kz>
2025-02-10 03:21:02 +0500

    0

---
+++ W3/lab1/06.variable/04.outputvariables.py
@@ -0,0 +1,36 @@
+#1.Output Variables
+
+#The Python print() function is often used to output variables.
+
+#Example 1
+x = "A"
+print(x)
+
+#Example 2
+x = "a"
+y = "b"
+z = "c"
+print(x, y, z)
+
+#Example 3
+x = "a "
+y = "b "
+z = "c"
+print(x + y + z)
+
+#Example 4
+x = 7
+y = 8
+print(x + y)
+
+#Error Example 1
+"""
+x = 5
+y = "John"
+print(x + y)
+"""
+
+#Example 5
+x = 5
+y = "John"
+print(x, y)
