38f4194f18838e74c2c3ed4574bed28ac4ef3949
Olzhas <o_nasseken@kbtu.kz>
2025-02-10 03:33:56 +0500

    0

---
+++ W3/lab1/10.stings/03.modifystrings.py
@@ -0,0 +1,39 @@
+#1.Upper Case
+
+#The upper() method returns the string in upper case:
+
+#Example 1
+a = "Hello, Python!"
+print(a.upper())
+
+#2.Lower Case
+
+#The lower() method returns the string in lower case:
+
+#Example 1
+a = "Hello, Python!"
+print(a.lower())
+
+#3.Remove Whitespace
+
+#The strip() method removes any whitespace from the beginning or the end:
+
+#Example 1
+a = " Hello, Python! "
+print(a.strip()) # returns "Hello, Python!"
+
+#4.Replace String
+
+#The replace() method replaces a string with another string:
+
+#Example 1
+a = "Hello, Python!"
+print(a.replace("H", "T"))
+
+#5.Split String
+
+#The split() method returns a list where the text between the specified separator becomes the list items.
+
+#Example 1
+a = "Hello, Python!"
+print(a.split("o")) # returns ['Hell', ', Pyth', 'n!']
