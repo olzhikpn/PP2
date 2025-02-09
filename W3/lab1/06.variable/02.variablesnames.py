d11328211992a5fa276e8c15edbf7c4da75f143a
Olzhas <o_nasseken@kbtu.kz>
2025-02-10 03:12:18 +0500

    0

---
+++ W3/lab1/06.variable/02.variablesnames.py
@@ -0,0 +1,35 @@
+#1.Variable Names
+
+"""
+A variable name must start with a letter or the underscore character
+A variable name cannot start with a number
+A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+Variable names are case-sensitive (age, Age and AGE are three different variables)
+A variable name cannot be any of the Python keywords.
+"""
+
+#Example 1
+myvar = "Apple"
+my_var = "Apple"
+_my_var = "Apple"
+myVar = "Apple"
+MYVAR = "Apple"
+myvar2 = "Apple"
+
+#Error Example 2
+"""
+2myvar = "Apple"
+my-var = "Apple"
+my var = "Apple"
+"""
+
+#2.Multi Words Variable Names
+
+#Camel Case
+myVariableName = "Apple"
+
+#Pascal Case
+MyVariableName = "Apple"
+
+#Snake Case
+my_variable_name = "Apple"
