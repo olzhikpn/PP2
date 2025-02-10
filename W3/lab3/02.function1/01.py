0e2947ae9f24ccaa19429fc5b6a08ba58099830f
Olzhas <o_nasseken@kbtu.kz>
2025-02-10 09:15:28 +0500

    0

---
+++ W3/lab3/02.function1/01.py
@@ -0,0 +1,13 @@
+from math import sqrt
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+prime_numbers = list(filter(lambda x: is_prime(x), numbers))
+print("Prime numbers:", prime_numbers)
