1faf1f22dfdb7dd28b890f0860bbdd419420cb20
Olzhas <o_nasseken@kbtu.kz>
2025-02-10 09:23:21 +0500

    0

---
+++ W3/lab3/02.function1/11.py
@@ -0,0 +1,8 @@
+def is_palindrome(s):
+    s = s.replace(" ", "").lower() 
+    return s == s[::-1]
+
+#Example
+print(is_palindrome("madam")) 
+print(is_palindrome("hello"))  
+print(is_palindrome("A Santa at NASA"))
