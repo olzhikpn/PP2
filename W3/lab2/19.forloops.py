bf1b3ae69ebce1537c3f292c38a86289aaeb3710
Olzhas <o_nasseken@kbtu.kz>
2025-02-10 07:42:16 +0500

    0

---
+++ W3/lab2/19.forloops.py
@@ -0,0 +1,123 @@
+#1.Python For Loops
+
+"""
+A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string).
+
+This is less like the for keyword in other programming languages, and works more like an iterator method as found in other object-orientated programming languages.
+
+With the for loop we can execute a set of statements, once for each item in a list, tuple, set etc.
+
+"""
+
+#Example 1
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+
+#The for loop does not require an indexing variable to set beforehand.
+
+#2.Looping Through a String
+
+#Even strings are iterable objects, they contain a sequence of characters:
+
+#Example 1
+for x in "banana":
+    print(x)
+
+#3.The break Statement
+
+#With the break statement we can stop the loop before it has looped through all the items:
+
+#Example 1
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+    if x == "banana":
+        break
+
+#Example 2 
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    if x == "banana":
+        break
+    print(x)
+
+#3.The continue Statement
+
+#With the continue statement we can stop the current iteration of the loop, and continue with the next:
+
+#Example 1
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    if x == "banana":
+        continue
+    print(x)
+
+#4.The range() Function
+
+"""
+To loop through a set of code a specified number of times, we can use the range() function,
+
+The range() function returns a sequence of numbers, starting from 0 by default, and increments by 1 (by default), and ends at a specified number.
+
+"""
+
+#Example 1
+for x in range(6):
+    print(x)
+
+#The range() function defaults to 0 as a starting value, however it is possible to specify the starting value by adding a parameter: range(2, 6), which means values from 2 to 6 (but not including 6):
+
+#Example 2
+for x in range(2, 6):
+    print(x)
+
+#The range() function defaults to increment the sequence by 1, however it is possible to specify the increment value by adding a third parameter: range(2, 30, 3):
+
+#Example 3
+for x in range(2, 30, 3):
+    print(x)
+
+#5.Else in For Loop
+
+#The else keyword in a for loop specifies a block of code to be executed when the loop is finished:
+
+#Example 1
+for x in range(6):
+    print(x)
+else:
+    print("Finally finished!")
+
+#The else block will NOT be executed if the loop is stopped by a break statement.
+
+#Example 2
+for x in range(6):
+    if x == 3: break
+    print(x)
+else:
+    print("Finally finished!")
+
+#6.Nested Loops
+
+"""
+A nested loop is a loop inside a loop.
+
+The "inner loop" will be executed one time for each iteration of the "outer loop":
+
+"""
+
+#Example 1
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+    for y in fruits:
+        print(x, y)
+
+#7.The pass Statement
+
+#for loops cannot be empty, but if you for some reason have a for loop with no content, put in the pass statement to avoid getting an error.
+
+#Example 1
+for x in [0, 1, 2]:
+    pass
